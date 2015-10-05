ae42c5ac443e47af4f737a6e245e5b1e17fdb551
Masato Hagiwara <masato@duolingo.com>
2015-10-04 20:22:16 -0400

    Modify project.clj for new dependency.

--- sisku/project.clj
+++ sisku/project.clj
@@ -3,6 +3,7 @@
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [compojure "1.0.0"]
-                 [congomongo "0.1.8"]]
+                 [congomongo "0.1.8"]
+		 [lein-ring "0.4.6"]]
   :dev-dependencies [[lein-ring "0.4.6"]]
   :main misc.sisku.core)
